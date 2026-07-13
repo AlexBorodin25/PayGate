19355c5483730c592cb2df94d01938c592d78bda
alexb <aborod1@wgu.edu>
2026-07-13 16:47:45 -0400

    added __pycache__/ to .gitignore, changed Stripe_webhook_url to ..._SECRET

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     stripe_secret_key: str = Field(alias="STRIPE_SECRET_KEY")
-    stripe_webhook_url: str = Field(alias="STRIPE_WEBHOOK_URL")
+    stripe_webhook_secret: str = Field(alias="STRIPE_WEBHOOK_SECRET")
     database_url: str = Field(alias="DATABASE_URL")
     app_base_url: str = Field(alias="APP_BASE_URL")
     orders_api_key: str = Field(alias="ORDERS_API_KEY")
